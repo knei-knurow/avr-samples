feb89b59e4d24802dac210b763337b389b190af3
Bartek Pacia <barpac02@gmail.com>
2020-11-07 13:09:55 +0100

    blink 8 works like a champ heheeee

--- src/blink_8.c
+++ src/blink_8.c
@@ -3,13 +3,13 @@
 #include <util/delay.h>
 
 int main(void) {
-  DDRB = 7;  // set all pins to OUT
+  DDRB = 0b11111111;  // set all pins to OUT
 
   while (1) {
     PORTB = 1;
     for (int i = 0; i < 8; i++) {
+      _delay_ms(500);  // delay has to be first (because of pin0)
       PORTB = PORTB << 1;
-      _delay_ms(500);
     }
   }
 }
