89ec6365c6aa85a1fe42b632fb6a701b8dc0e4a8
Bartek Pacia <barpac02@gmail.com>
2021-01-06 19:28:49 +0100

    use PB0 for blink (1)

--- blink/blink.c
+++ blink/blink.c
@@ -2,12 +2,12 @@
 #include <util/delay.h>
 
 int main(void) {
-  DDRB = DDRB | (1 << PB1);
+  DDRB = DDRB | (1 << PB0);
   // DDRB = Data Direction Register for port B.
   // Setting PIN1 on PORTB to 1. 1 means it is an output pin.
 
   while (1) {
     _delay_ms(1000);
-    PORTB = PORTB ^ (1 << PB1);
+    PORTB = PORTB ^ (1 << PB0);
   }
 }
